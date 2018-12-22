06f175110003ab4af085d4660ef051dd64e0beff
Ulf Prill <cataurusfynn@yahoo.de>
2018-12-22 07:31:36 +0100

    add AssemblyInformationalVersion

---
+++ NetStandard/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+#if NETSTANDARD1_0
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+#else
+	#if NETSTANDARD1_1
+	[assembly: AssemblyInformationalVersion("1.1.0.0")]
+	#else
+		#if NETSTANDARD1_2
+		[assembly: AssemblyInformationalVersion("1.2.0.0")]
+		#else
+			#if NETSTANDARD1_3
+			[assembly: AssemblyInformationalVersion("1.3.0.0")]
+			#else
+				#if NETSTANDARD2_0
+				[assembly: AssemblyInformationalVersion("2.0.0.0")]
+				#endif
+			#endif
+		#endif
+	#endif
+#endif
