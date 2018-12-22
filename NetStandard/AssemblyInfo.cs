aa4df8234d8cf495b254950ed525987ef1ab3fcd
Ulf Prill <cataurusfynn@yahoo.de>
2018-12-22 08:06:02 +0100

    pragma warning disable CS7035

--- NetStandard/AssemblyInfo.cs
+++ NetStandard/AssemblyInfo.cs
@@ -1,10 +1,14 @@
 ﻿using System.Reflection;
 
+#pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
+[assembly: AssemblyFileVersion("1.0.0.0")]
+#pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
+
 #if NETSTANDARD1_0
 [assembly: AssemblyInformationalVersion("1.0.0.0")]
 #else
-	#if NETSTANDARD1_1
-	[assembly: AssemblyInformationalVersion("1.1.0.0")]
+#if NETSTANDARD1_1
+[assembly: AssemblyInformationalVersion("1.1.0.0")]
 	#else
 		#if NETSTANDARD1_2
 		[assembly: AssemblyInformationalVersion("1.2.0.0")]
